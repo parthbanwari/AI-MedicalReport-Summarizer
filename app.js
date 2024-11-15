8465a4c9738ef4f488f604d56cb827cd45a3d0ef
parthbanwari <118819641+parthbanwari@users.noreply.github.com>
2024-11-16 00:47:02 +0530

    F

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 // Start the server
-Const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('Server started on http://localhost:3000');
 });
